93ca0a6b36448238b2513e7d6ab63d5070e39712
FaridIzquierdo <89101074+FaridIzquierdo@users.noreply.github.com>
2024-06-29 17:11:08 -0500

    Implement Screen Register Step 2 and 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:munaymed_frontend/screens/screen-login.dart';
 import 'package:munaymed_frontend/screens/screen-register-step-one.dart';
+import 'package:munaymed_frontend/screens/screen-register-step-three.dart';
+import 'package:munaymed_frontend/screens/screen-register-step-two.dart';
 import 'package:munaymed_frontend/screens/screen-register.dart';
 
 void main() {
@@ -20,7 +22,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ScreenRegisterStepOne(),
+      home: ScreenRegisterStepThree(),
+
       
     );
   }
